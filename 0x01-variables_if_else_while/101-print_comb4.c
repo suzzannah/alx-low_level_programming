913ae9a581d2db20b354839703d730d5b2454aac
suzzannah <susanwa8@gmail.com>
2023-03-17 09:12:39 -0700

    a program that prints all possible different combinations of three digits.

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * main -  a program that prints all possible different combinations of three digits.
+ * main - a program that prints all possible combinations of three digits.
  *
  * Return: Always 0.
  */
