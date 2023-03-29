c99fec4da0ebcd9320f2db4842c6bf4cea7d843e
suzzannah <susanwa8@gmail.com>
2023-03-29 10:23:51 -0700

    It is the addition of strangeness to beauty that constitutes the romantic character in art

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -4,7 +4,9 @@
  * infinite_add - adds two numbers
  * @n1: first number
  * @n2: second number
+ * @r: buffer for result
  * @size_r: buffer size
+ * Lets see how we solve this
  * Return: address of r or 0
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
