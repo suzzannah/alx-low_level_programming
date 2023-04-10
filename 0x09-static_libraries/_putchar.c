f579c6dd3c0246825de330b258787d2ee1e8bbf5
suzzannah <susanwa8@gmail.com>
2023-04-10 13:21:15 -0700

    create_static_lib.sh

--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -3,7 +3,14 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
+
+/**
+ * _putchar -this is the entry point
+ * @c: param -> character to print
+ * return: 1 if success and -1 if error
+ */
+
 int _putchar(char c)
 {
-return (0);
+	return (write(1, &c, 1));
 }
