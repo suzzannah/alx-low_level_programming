de51cc1098d1def561ec406b22cc3c6d40ce52c8
suzzannah <susanwa8@gmail.com>
2023-04-10 12:56:36 -0700

    another change

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,18 +1,10 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
 
-/**
- * _puts - prints a string
- * @str: the string
- * Return: the length of the string
- */
-void _puts(char *str)
+void _puts(char *s)
 {
-	int i = 0;
-
-	while (str[i] != '\0')
-	{
-		_putchar(str[i]);
-		i++;
-	}
-	_putchar('\n');
+puts(s);
 }
